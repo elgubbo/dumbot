3513a07327671ed8bdc8be6575afcfed2cb3defc
Alexander 'elgubbo' Reichert <a.reichert@webmelt.de>
2016-04-25 10:57:04 +0200

    added examples to config

--- config_example.js
+++ config_example.js
@@ -1,7 +1,12 @@
-exports config;
-var config = {
-	token : {
+exports.token = {
 		slack : "token",
 		facebook: "token"
-	}
+	};
+
+exports.db = {
+	jsonPath: './storage/'
 };
+
+exports.botPath = './bots/';
+
+exports.superAdmin = 'USERID';
